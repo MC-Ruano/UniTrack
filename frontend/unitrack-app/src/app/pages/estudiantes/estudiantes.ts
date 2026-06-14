90b24daf554697421303ec0ad3c01d67979c3a83
marcos494489 <102492874+marcos494489@users.noreply.github.com>
2026-06-14 16:31:18 -0600

    Frontend estudiantes conectado

--- frontend/unitrack-app/src/app/pages/estudiantes/estudiantes.ts
+++ frontend/unitrack-app/src/app/pages/estudiantes/estudiantes.ts
@@ -1,9 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-estudiantes',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './estudiantes.html',
   styleUrl: './estudiantes.css',
 })
-export class Estudiantes {}
+export class Estudiantes implements OnInit {
+
+  estudiantes: any[] = [];
+
+  nuevo = {
+    carnet: '',
+    nombre: '',
+    apellido: '',
+    correo: '',
+    fechaNacimiento: '',
+    carrera: '',
+    semestreActual: 1
+  };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.cargarEstudiantes();
+  }
+
+  cargarEstudiantes() {
+
+    this.http
+      .get<any[]>('http://localhost:3000/api/estudiantes')
+      .subscribe(data => {
+
+        this.estudiantes = data;
+
+      });
+
+  }
+
+  agregarEstudiante() {
+
+    this.http
+      .post(
+        'http://localhost:3000/api/estudiantes',
+        this.nuevo
+      )
+      .subscribe(() => {
+
+        this.cargarEstudiantes();
+
+        this.nuevo = {
+          carnet: '',
+          nombre: '',
+          apellido: '',
+          correo: '',
+          fechaNacimiento: '',
+          carrera: '',
+          semestreActual: 1
+        };
+
+      });
+
+  }
+
+}
